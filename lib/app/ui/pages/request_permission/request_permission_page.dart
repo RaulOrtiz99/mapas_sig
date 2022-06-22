fea9f04a7baa9c993daeb28d807aaea84320e9ec
RaulOrtiz99 <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>
2022-06-22 16:30:26 -0400

    agregando permisos para acceder a la ubicacion del dispositivo

---
+++ lib/app/ui/pages/request_permission/request_permission_page.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mapas/app/ui/pages/request_permission/request_permission_controller.dart';
+import 'package:mapas/app/ui/routes/routes.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class RequestPermissionPage extends StatefulWidget {
+  const RequestPermissionPage({Key? key}) : super(key: key);
+
+  @override
+  State<RequestPermissionPage> createState() => _RequestPermissionPageState();
+}
+
+class _RequestPermissionPageState extends State<RequestPermissionPage> {
+  final _controller = RequestPermissionController(Permission.locationWhenInUse);
+  late StreamSubscription _subscription;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+   _subscription= _controller.onStatusChanged.listen((status) {
+     if(status ==PermissionStatus.granted){
+       Navigator.pushReplacementNamed(context, Routes.Home);
+     }
+    },
+    );
+
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          alignment: Alignment.center,
+          child: ElevatedButton(
+            child: const Text("Allow"),
+            onPressed: (){
+              _controller.request();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/ui/routes/routes.dart
@@ -0,0 +1,10 @@
+
+
+abstract class  Routes{
+
+  static const SPLASH = '/';
+  static const Home = '/home';
+  static const PERMISSION = '/permissions';
+
+
+}
